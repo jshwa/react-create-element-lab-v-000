7661d05c34c434bafa6585daf5fb1f2185e5a85e
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-30 22:33:35 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const meInReact =
     React.createElement('h1', {}, 'An Awesome Person'),
     React.createElement('p', {}, 'Who is learning React'),
     React.createElement('ul', {className:'my-interests'}, [
-      React.createElement('li', {}, 'Javascript'),
+      React.createElement('li', {}, 'JavaScript'),
       React.createElement('li', {}, 'React'),
       React.createElement('li', {}, 'Movies'),
       React.createElement('li', {}, 'Ice cream')
